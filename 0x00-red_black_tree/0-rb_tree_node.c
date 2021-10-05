91340f54340c030b9de4d753074741ad60da7c84
Stephen <nehpets0701@gmail.com>
2021-10-05 15:56:43 -0400

    repo

---
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,13 @@
+#include "rb_trees.h"
+
+/**
+*rb_tree_node-makes new node
+*Return:pointer to node or NULL
+*@parent:parent
+*@value:value
+*@color:color
+*/
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+
+}
