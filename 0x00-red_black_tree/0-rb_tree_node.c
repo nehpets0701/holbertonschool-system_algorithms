744bacedade9a5665083e55aeaa502eab1c3b992
Stephen <nehpets0701@gmail.com>
2021-10-06 13:57:44 -0400

    zero

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -9,5 +9,15 @@
 */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
+	rb_tree_t *new;
 
+	new = malloc(sizeof(rb_tree_t))
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->color = color;
+	new->left = NULL;
+	new->right = NULL;
 }
