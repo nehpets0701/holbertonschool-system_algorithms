e5d4a0b2d68905164113e3eb230fd5d922bd270f
Stephen <nehpets0701@gmail.com>
2022-02-22 23:17:47 -0500

    betty

--- 0x04-nary_trees/nary_trees.h
+++ 0x04-nary_trees/nary_trees.h
@@ -16,11 +16,11 @@
  */
 typedef struct nary_tree_s
 {
-    char            *content;
-    struct nary_tree_s  *parent;
-    size_t          nb_children;
-    struct nary_tree_s  *children;
-    struct nary_tree_s  *next;
+	char            *content;
+	struct nary_tree_s  *parent;
+	size_t          nb_children;
+	struct nary_tree_s  *children;
+	struct nary_tree_s  *next;
 } nary_tree_t;
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
